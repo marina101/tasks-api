98cecc35a3c5f49e21c914c326942cc8e322ec02
marina101 <mchirc1@gmail.com>
2018-06-05 13:06:12 +0200

    Fixed up users api

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ app.get('/api/users/:id', (req, res) => {
 		return u.id === parseInt(req.params.id)
 	 })
 	
-	if (!user) {
-		res.status(404).send('The user with the given id was not found');
-		return;
-	}
+	if (!user) return res.status(404).send('The user with the given id was not found');
 
 	res.send(user);
 });
@@ -34,10 +31,7 @@ app.get('/api/users/:id', (req, res) => {
 app.post('/api/users', (req, res) => {
 	const { error } = validateUser(req.body);
 
-	if (error){
-		res.status(400).send(error);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	const user = {
 		id: users.length + 1,
@@ -53,21 +47,31 @@ app.put('/api/users/:id', (req, res) => {
 		return u.id === parseInt(req.params.id)
 	 })
 	
-	if (!user) {
-		res.status(404).send('The user with the given id was not found');
-		return;
-	}
+	if (!user) return res.status(404).send('The user with the given id was not found');
 
 	const { error } = validateUser(req.body);
 
-	if (error){
-		res.status(400).send(error);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	user.name = req.body.name
 	res.send(user);
 })
+
+app.delete('/api/users/:id', (req, res) => {
+	let user = users.find((u) => { 
+		return u.id === parseInt(req.params.id)
+	 })
+	
+	if (!user) return res.status(404).send('The user with the given id was not found');
+
+	const index = users.indexOf(user);
+	console.log('index of user ' + index);
+
+	users.splice(index, 1);
+	console.log('after deleteion, users array' + users);
+
+	res.send(user);
+});
 
 function validateUser(user) {
 	const schema = {
